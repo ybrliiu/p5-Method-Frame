9a6eeea3c878ac522a18e9dc2f79b3dca549066d
mp0liiu <raian@reeshome.org>
2019-02-10 22:58:43 +0900

    add optional parameter

--- lib/Mule/Meta/Method.pm
+++ lib/Mule/Meta/Method.pm
@@ -4,8 +4,10 @@ use warnings;
 use utf8;
 
 use Scalar::Util ();
-use Mule::Meta::Method::RequiredParameter;
+use Mule::Util;
 use Mule::Meta::Method::ListParameters;
+use Mule::Meta::Method::RequiredParameter;
+use Mule::Meta::Method::OptionalParameter;
 use Mule::Meta::Method::ReturnType;
 
 use Class::Accessor::Lite (
@@ -28,13 +30,63 @@ sub new {
 
 sub create_params {
     my ($class, $params) = @_;
-    my @params_objects = map { Mule::Meta::Method::RequiredParameter->new($_) } @$params;
-    Mule::Meta::Method::ListParameters->new(\@params_objects);
+    if (
+        Scalar::Util::blessed($params) &&
+        $params->isa('Mule::Meta::Method::Parameters')
+    ) {
+        $params;
+    }
+    elsif ( ref $params eq 'ARRAY' ) {
+        my @params_objects = map { Mule::Meta::Method::RequiredParameter->new($_) } @$params;
+        Mule::Meta::Method::ListParameters->new(\@params_objects);
+    }
+    elsif ( ref $params eq 'HASH' ) {
+    }
+    else {
+        Carp::confess 'Invalid parameters option passed.';
+    }
+}
+
+sub create_param {
+    my ($class, $param) = @_;
+    if (
+        Scalar::Util::blessed($param) && 
+        $param->isa('Mule::Meta::Method::Parameter')
+    ) {
+        $param;
+    }
+    elsif ( !(defined Mule::Util::ensure_type_constraint_object($param) ) ) {
+        Mule::Meta::Method::RequiredParameter->new($param);
+    }
+    elsif ( ref $param eq 'HASH' ) {
+        my $err = Mule::Util::ensure_type_constraint_object($param->{isa});
+        Carp::confess $err if defined $err;
+        if ( exists $param->{optional} ) {
+            Mule::Meta::Method::OptionalParameter->new($param->{isa});
+        }
+        elsif ( exists $param->{default} ) {
+            Mule::Meta::Method::OptionalParameter->new($param->{isa}, $param->{default});
+        }
+        else {
+            Mule::Meta::Method::RequiredParameter->new($param->{isa});
+        }
+    }
+    else {
+        Carp::confess 'Invalid parameter option passed.';
+    }
 }
 
 sub create_return_type {
     my ($class, $constraint) = @_;
-    Mule::Meta::Method::ReturnType->new($constraint);
+    if (
+        Scalar::Util::blessed($constraint) &&
+        $constraint->isa('Mule::Meta::Method::ReturnType')
+    ) {
+        $constraint;
+    }
+    else {
+        Mule::Meta::Method::ReturnType->new($constraint);
+    }
 }
 
 sub build {
--- lib/Mule/Meta/Method/OptionalParameter.pm
+++ lib/Mule/Meta/Method/OptionalParameter.pm
@@ -1,4 +1,4 @@
-package Mule::Meta::Method::RequiredParameter;
+package Mule::Meta::Method::OptionalParameter;
 use 5.014004;
 use strict;
 use warnings;
@@ -6,22 +6,30 @@ use utf8;
 
 use parent 'Mule::Meta::Method::Parameter';
 
+use Class::Accessor::Lite (
+  ro => [qw( maybe_default )],
+);
+
 use Carp ();
 use Mule::Util;
 
 sub new {
     Carp::croak 'Too few arguments' if @_ < 2;
-    my ($class, $constraint) = @_;
+    my ($class, $constraint, $maybe_default) = @_;
 
     my $err = Mule::Util::ensure_type_constraint_object($constraint);
     Carp::croak $err if defined $err;
 
-    bless +{ constraint => $constraint }, $class;
+    bless +{
+        constraint    => $constraint,
+        maybe_default => $maybe_default,
+    }, $class;
 }
 
 sub validate {
     Carp::croak 'Too few arguments' if @_ < 2;
-    my ($self, $argument) = @_;
+    my ($self, $maybe_argument) = @_;
+    my $argument = $maybe_argument // $self->maybe_default;
     $self->constraint->check($argument)
         ? ( $argument, undef )
         : ( undef, qq{Parameter type is mismatch. (Parameter type is '@{[ $self->constraint ]}' but Argument value is '$argument'.)} );
