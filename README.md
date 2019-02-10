a6f95d51843d185de4d0276146f9f80f4952d9f0
mp0liiu <raian@reeshome.org>
2019-02-10 14:31:10 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Mule - It's new $module
+
+# SYNOPSIS
+
+    use Mule;
+
+# DESCRIPTION
+
+Mule is ...
+
+# LICENSE
+
+Copyright (C) mp0liiu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+mp0liiu <raian@reeshome.org>
---
+++ lib/Mule.pm
@@ -0,0 +1,39 @@
+package Mule;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mule - It's new $module
+
+=head1 SYNOPSIS
+
+    use Mule;
+
+=head1 DESCRIPTION
+
+Mule is ...
+
+=head1 LICENSE
+
+Copyright (C) mp0liiu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+mp0liiu E<lt>raian@reeshome.orgE<gt>
+
+=cut
+
