ff62cbe049431de86ef03c513766f98521335840
mp0liiu <raian@reeshome.org>
2019-02-23 01:42:44 +0900

    create parameters

---
+++ lib/Method/Frame/Functions/CompareFrame/FramedMethod/Parameters.pm
@@ -0,0 +1,32 @@
+package Method::Frame::Functions::CompareFrame::FramedMethod::Parameters;
+
+use Method::Frame::Base;
+
+use Carp ();
+
+sub new { Carp::croak 'This is abstract method.' }
+
+sub _type { Carp::croak 'This is abstract method.' }
+
+sub _compare_type {
+    my ($self, $params) = @_;
+
+    $self->_type eq $params->_type
+        ? undef
+        : 'Parameters type is different. '
+            . "(@{[ $self->_type ]} parameters vs @{[ $params->_type ]} parameters)";
+}
+
+sub _compare_each_parameters { Carp::croak 'This is abstract method.' }
+
+sub compare {
+    my ($self, $params) = @_;
+
+    if ( my $err = $self->_compare_type ) {
+        return [ $err ];
+    }
+
+    $self->_compare_each_parameters($params);
+}
+
+1;
