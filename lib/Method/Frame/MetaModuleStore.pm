09c7ded0049c51adb0e896943a722e97c952135d
mp0liiu <raian@reeshome.org>
2019-02-28 02:44:46 +0900

    create module store

---
+++ lib/Method/Frame/MetaModuleStore.pm
@@ -0,0 +1,11 @@
+package Method::Frame::MetaModuleStore;
+
+use Method::Frame::Base;
+
+use Carp ();
+
+sub maybe_get { Carp::croak 'This is abstract method.' }
+
+sub store { Carp::croak 'This is abstract method.' }
+
+1;
