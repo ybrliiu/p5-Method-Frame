461d3cc677c853431b22e1334be4fca8c1a9f110
mp0liiu <raian@reeshome.org>
2019-05-30 21:23:28 +0900

    デフォルト値をつくる関数から別の引数を見れるようにする

--- lib/Method/Frame/Domain/FramedMethodBuilder/ListParameters.pm
+++ lib/Method/Frame/Domain/FramedMethodBuilder/ListParameters.pm
@@ -25,15 +25,59 @@ sub new {
 }
 
 sub validate {
-    my ($self, @args) = @_;
+    my ($self, @orig_args) = @_;
 
     my $num = $self->{num};
-    return ( undef, 'Too few args' ) if scalar @args < $num;
-    return ( undef, 'Too many args' ) if scalar @args > $num;
+
+    # 仮実装
+    # r, r, d1, d2
+    # (1, 1, 1) = (1, 1, 1, d2)
+    # (1, 1)    = (1, 1, d1, d2)
+    # r, d1, r, d2
+    # (1, 1, 1) = (1, 1, 1, d2)
+    # (1, 1)    = (1, d1, 1, d2)
+    # d1, r, d2, r
+    # (1, 1, 1) = (1, 1, d2, 1)
+    # (1, 1)    = (d1, 1, d2, 1)
+    # d1, r, d2, r, d3
+    # (1, 1, 1, 1) = (1, 1, 1, 1, d3)
+    # (1, 1, 1)    = (1, 1, d2, 1, d3)
+    # (1, 1)       = (d1, 1, d2, 1, d3)
+    # デフォルト引数の位置を記録
+    # デフォルト引数のリストを後方から順番に埋める
+
+    return ( undef, 'Too many args' ) if @orig_args > $num;
+
+    my $required_params = $self->{required_params} //= do {
+        my @required_params =
+            grep { $_->isa('Method::Frame::Domain::FramedMethodBuilder::RequiredParameter') }
+            @{ $self->{list} };
+        \@required_params;
+    };
+
+    return ( undef, 'Too few args' ) if @orig_args < @$required_params;
+
+    my $default_param_indexes = $self->{default_param_indexes} //= do {
+        my @default_param_indexes =
+            grep { $self->{list}[$_]->isa('Method::Frame::Domain::FramedMethodBuilder::DefaultParameter') }
+            0 .. $#{ $self->{list} };
+        \@default_param_indexes;
+    };
+
+    my $num_diff = @{ $self->{list} } - @orig_args;
+
+    my @use_default_param_indexes =
+        @$default_param_indexes[ $#$default_param_indexes - $num_diff + 1 .. $#$default_param_indexes ];
+
+    my @args;
+    $args[$_] = $self->{list}[$_]{default} for reverse @use_default_param_indexes;
+    for my $i (0 .. $#{ $self->{list} } - $num_diff) {
+      $args[$i] = shift @orig_args unless defined $args[$i];
+    }
 
     my @meta_params = @{ $self->{list} };
     my @valid_args = map {
-        my ($valid_arg, $err) = $meta_params[$_]->validate($args[$_]);
+        my ($valid_arg, $err) = $meta_params[$_]->validate($args[$_], \@args);
         if ( defined $err ) {
             return ( undef, "${_}Th $err" );
         }
